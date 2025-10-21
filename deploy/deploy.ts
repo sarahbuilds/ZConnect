443106b90d6ceec07ad1c481a59d94dbc3f2d28f
sarahbuilds <AllendeTarrance21061982glf@hotmail.com>
2025-10-21 16:20:30 +0800

    update

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedInterestMatcher = await deploy("InterestMatcher", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`InterestMatcher contract: `, deployedInterestMatcher.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_interestMatcher"; // id required to prevent reexecution
+func.tags = ["InterestMatcher"];
